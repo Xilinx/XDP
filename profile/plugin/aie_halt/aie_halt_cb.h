34309f7c68cf90f4719bbbd84ecd7bcc158d42be
richard-byron <richard.byron@amd.com>
2026-09-08 16:23:18 -0700

    Missing header from last commit

--- profile/plugin/aie_halt/aie_halt_cb.h
+++ profile/plugin/aie_halt/aie_halt_cb.h
@@ -25,9 +25,7 @@ extern "C" {
 
   XDP_PLUGIN_EXPORT void updateDeviceAIEHalt(void* hwCtxImpl);
   XDP_PLUGIN_EXPORT void finishflushDeviceAIEHalt(void* hwCtxImpl);
-  XDP_PLUGIN_EXPORT void aieHaltRunStart(void* run_impl_ptr, void* hwctx,
-                                         uint32_t run_uid,
-                                         const char* kernel_name);
+  XDP_PLUGIN_EXPORT void aieHaltRunStart(void* hwctx, const char* kernel_name);
 
 }
 #endif
